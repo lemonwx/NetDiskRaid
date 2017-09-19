686a9f4e7b8bacc106421245d86daad3ad1cda63
lim <wxgwmmoc@126.com>
2017-09-19 09:27:17 +0800

    adjust dir

--- urls.py
+++
@@ -1,6 +0,0 @@
-#the file or dictionary under home page 
-urlHomeDirFileOrDictionaryList = "http://pan.baidu.com/api/list"
-
-#the file or dictionary under a dictionary (by params dictionaryName)
-#zhe li wei le shixian 
-urlFileListofDictionary = "https://pan.baidu.com/api/list?dir=%2Flmwx"%
